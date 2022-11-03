27d295257acf580b49d5a77e2dda19988a7ce74d
snednarb <brandenvanessa@gmail.com>
2022-11-03 09:36:05 -0400

    Update discovery and collection scripts

--- mount-nfs-discovery.sh
+++ mount-nfs-discovery.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-nfs_mounts=($(findmnt -lo target -n -t nfs4 | cut -f2 -d':'))
-#nfs_mounts=$(($nfs_mounts-1))
-echo "{" 
-echo "\"data\":[" 
-for nfs_mount in ${nfs_mounts[@]} 
+[[ "$1" != "" ]] && TYPE=$1 || TYPE=nfs
+
+nfs_mounts=($(findmnt -lo target -n -t $TYPE | cut -f2 -d':'))
+echo -n "{\"data\":["
+for nfs_mount in ${nfs_mounts[@]}
 do
-    echo -n "    {\"{#MOUNT_POINT}\":\"$nfs_mount\"}," 
-done | sed 's/,$//' 
+    echo -n "{\"{#MOUNT_POINT}\":\"$nfs_mount\"},"
+done | sed 's/,$//'
 # Added sed in the end of for loop, because old zabbix doesn't accept json with trailing commas
 
-echo "]" 
-echo "}" 
-
+echo "]}"
--- nfsiostat-check.sh
+++ nfsiostat-check.sh
@@ -1,61 +1,52 @@
 #!/bin/bash
 ##################################
-# Zabbix monitoring script
-#
-# iostat:
-#  - IO
-#  - running / blocked processes
-#  - swap in / out
-#  - block in / out
-#
-# Info:
-#  - vmstat data are gathered via cron job
+# Zabbix monitoring script for nfs
 ##################################
 # Contact:
 #  vincent.viallet@gmail.com
 ##################################
 # ChangeLog:
 #  20100922     VV      initial creation
+#  20221103     BS      modernize for Zabbix 4.0+
 ##################################
 
 # Zabbix requested parameter
-ZBX_REQ_DATA="$2"
-ZBX_REQ_DATA_MOUNT_POINT="$1"
+MOUNT_POINT="$1"
 
-# source data file
-NFS_IO_STAT_CMD=$(nfsiostat $ZBX_REQ_DATA_MOUNT_POINT | sed "1,4d" | grep -v read | grep -v write | awk '{print}' ORS=' ')
-# Error handling:
-#  - need to be displayable in Zabbix (avoid NOT_SUPPORTED)
-#  - items need to be of type "float" (allow negative + float)
-#
-ERROR_NO_DATA_FILE="-0.9900"
-ERROR_OLD_DATA="-0.9901"
-ERROR_WRONG_PARAM="-0.9902"
-ERROR_MISSING_PARAM="-0.9903"
+NFS_IO_STAT_CMD=$(nfsiostat "$MOUNT_POINT" 2>/dev/null)
+EXIT_CODE=$?
+NFS_IO_STAT_CMD=$(echo "$NFS_IO_STAT_CMD" | sed "1,4d" | grep -v read | grep -v write | awk '{print}' ORS=' ')
 
-# Missing device to get data from
-if [ -z "$ZBX_REQ_DATA_MOUNT_POINT" ]; then
-  echo $ERROR_MISSING_PARAM
-  exit 1
+JSON_BUFFER="{ \"mountpoint\":\"$MOUNT_POINT\", \"error\":\""
+
+if [[ -z "$MOUNT_POINT" ]]; then
+  # Missing required parameter
+  JSON_BUFFER+="Required parameter not provided\""
+elif [[ "$EXIT_CODE" == "127" ]]; then
+  # Command not found
+  JSON_BUFFER+="Command nfsiostat not found\""
+elif [[ -z "$NFS_IO_STAT_CMD" ]]; then
+  # nfiostat output empty
+  JSON_BUFFER+="Failed to fetch counters from nfsiostat\""
+else
+  JSON_BUFFER+="\", "
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"ops_sec\":\"" $1 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"rpc_backlog\":\"" $2 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"read_ops_sec\":\"" $3 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"read_kB_sec\":\"" $4 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"read_kB_op\":\"" $5 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"read_retrans\":\"" $6 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"read_RTT\":\"" $8 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"read_exe\":\"" $9 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"write_ops_sec\":\"" $10 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"write_kB_sec\":\"" $11 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"write_kB_op\":\"" $12 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"write_retrans\":\"" $13 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"write_RTT\":\"" $15 "\", "}')
+  JSON_BUFFER+=$(echo "$NFS_IO_STAT_CMD" | awk '{print "\"write_exe\":\"" $16 "\""}')
 fi
 
-
-case $ZBX_REQ_DATA in
-  rpcbacklog) echo $NFS_IO_STAT_CMD | awk '{print $2}' ;;
-  rretrans) echo $NFS_IO_STAT_CMD | awk '{print $6}' ;;
-  wretrans) echo $NFS_IO_STAT_CMD | awk '{print $13}' ;;
-  rkB/s) echo $NFS_IO_STAT_CMD | awk '{print $4}' ;;
-  wkB/s) echo $NFS_IO_STAT_CMD | awk '{print $11}' ;;
-  rkB/ops) echo $NFS_IO_STAT_CMD | awk '{print $5}' ;;
-  wkB/ops) echo $NFS_IO_STAT_CMD | awk '{print $12}' ;;
-  rRTT) echo $NFS_IO_STAT_CMD | awk '{print $8}' ;;
-  rEXE) echo $NFS_IO_STAT_CMD | awk '{print $9}' ;;
-  wRTT) echo $NFS_IO_STAT_CMD | awk '{print $15}' ;;
-  wEXE) echo $NFS_IO_STAT_CMD | awk '{print $16}' ;;
-  ops/s)   echo $NFS_IO_STAT_CMD | awk '{print $1}' ;;
-  rops/s)   echo $NFS_IO_STAT_CMD | awk '{print $3}' ;;
-  wops/s)      echo $NFS_IO_STAT_CMD | awk '{print $10}';; 
-  *) echo $ERROR_WRONG_PARAM; exit 1;;
-esac
+JSON_BUFFER+=" }"
+echo "$JSON_BUFFER"
 
 exit 0
